42c849a261f8eb5b1e23b159e52ee69d3e52214c
Zach Butler <zach.butler@eosnetwork.com>
2023-08-21 17:18:43 -0400

    Put code block back

--- handler.js
+++ handler.js
@@ -4,14 +4,16 @@ const api = axios.create({
     baseURL: 'https://api.telegram.org/bot',
 });
 
-module.exports.hello = async (event) => ({
-    statusCode: 200,
-    body: JSON.stringify(
-        {
-            message: 'Go Serverless v1.0! Your function executed successfully!',
-            input: event,
-        },
-        null,
-        2,
-    ),
-});
+module.exports.hello = async (event) => {
+    return {
+        statusCode: 200,
+        body: JSON.stringify(
+            {
+                message: 'Go Serverless v1.0! Your function executed successfully!',
+                input: event,
+            },
+            null,
+            2,
+        ),
+    };
+};

7e88a29804c1220bff0d1de497326aae4793c332
Zach Butler <zach.butler@eosnetwork.com>
2023-08-21 18:28:58 -0400

    Section comments

--- handler.js
+++ handler.js
@@ -21,16 +21,16 @@ module.exports.hello = async (event) => {
     } else {
         console.log(`Found ${chatId.length} char TELEGRAM_CHAT_ID "${chatId.slice(0, 2)}...${chatId.slice(-4)}" in the environment.`);
     }
-
+    // message contents
     const message = 'Test message from JavaScript.';
-
+    // send message to Telegram
     const response = await tgAPI.get(route, {
         params: {
             chat_id: chatId,
             text: message,
         },
     });
-
+    // construct useful data to return
     const result = {
         input: event,
         message,
@@ -40,7 +40,7 @@ module.exports.hello = async (event) => {
             status: response.status || null,
         },
     };
-
+    // sanitize and return useful information
     return {
         statusCode: 200,
         body: JSON.stringify(result).replace(tgKey, '${TELEGRAM_API_KEY}').replace(chatId, '${TELEGRAM_CHAT_ID}'), // eslint-disable-line no-template-curly-in-string

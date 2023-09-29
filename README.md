2427ea9f47b05dd6f32e338049794c82180413a5
Zach Butler <zach.butler@eosnetwork.com>
2023-09-28 23:57:30 -0400

    Write a build script

--- README.md
+++ README.md
@@ -7,6 +7,7 @@
     1. [Initialization](#initialization)
     1. [Lint](#lint)
     1. [Test](#test)
+    1. [Build](#build)
 1. [Inputs](#inputs)
     1. [Environment Variables](#environment-variables)
     1. [Events](#events)
@@ -55,6 +56,15 @@ This project uses the [jest](https://jestjs.io) test framework.
 yarn test
 ```
 The goal is full test coverage, not because we chased a number, but because we exhaustively tested all intended functionality.
+
+### Build
+This is how release artifacts are generated.
+```bash
+yarn build
+```
+The "build" generates a `*.zip` archive in the root of the repo that can be uploaded directly to AWS Lambda using the web console, AWS CLI, or with something like ~~Terraform~~ Tofu.
+
+The output of `yarn pack` is **_not_** compatible with AWS. AWS requires the dependencies (`node_modules`) to be packed in the `*.zip` file for lambdas, so it may be wise to do your own build with updated dependencies to make sure your deployment is not missing any security patches published for dependencies since our latest release. If you are building a tag, the script requires the version in the `git` tag to match the version in the `package.json`. Finally, the build script does briefly move your `node_modules` folder in order to guarantee developer dependencies are not packed into the `*.zip` file so it is as small as possible. The script puts your `node_modules` back afterwards so this should hopefully not be a problem for anyone.
 
 ## Inputs
 This lambda receives two primary inputs, AWS Simple Notification Service (SNS) event payloads delivered by AWS and user-defined environment variables to configure the script.
---
+++ scripts/build.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+set -eo pipefail
+
+function ee()
+{
+    echo "$ $*"
+    eval "$@" || :
+}
+
+function sanitize()
+{
+    printf "$*" | sed 's/[^-._a-zA-Z0-9]/-/g' | tr -s '-'
+}
+
+echo "Starting build. - ${BASH_SOURCE[0]}"
+NPM_ROOT="$(npm run env | grep '^PWD' | cut -d '=' -f '2')"
+pushd "$NPM_ROOT"
+# package info
+PACKAGE_NAME="$(cat package.json | jq -r '.name')"
+PACKAGE_VERSION="$(cat package.json | jq -r '.version')"
+echo "Found package.json for \"$PACKAGE_NAME\" version \"$PACKAGE_VERSION\"."
+# git info
+GIT_BRANCH="$(git branch --show-current)"
+GIT_COMMIT="$(git rev-parse HEAD)"
+GIT_SHORT_COMMIT="$(git rev-parse --short HEAD)"
+GIT_TAG="$(git --no-pager tag --points-at HEAD)"
+SANITIZED_BRANCH="$(sanitize "$GIT_BRANCH")"
+SANITIZED_TAG="$(sanitize "$GIT_TAG")"
+# verify tag matches package.json version, if it exists
+if [[ -n "$GIT_TAG" && "$GIT_TAG" != "v$PACKAGE_VERSION" ]]; then
+    printf '\e[1;31mERROR: The git tag does not match the package.json version!\e[0m\n'
+    echo "             git tag: $GIT_TAG"
+    echo "package.json version: $PACKAGE_VERSION"
+    echo 'These must match to build a release. Rejecting build.'
+    exit 10
+fi
+# backup node_modules
+unset UNIX_TIME
+if [[ -d node_modules ]]; then
+    echo 'Backing up your node_modules folder. It will be restored after the build.'
+    UNIX_TIME="$(date +%s)"
+    ee "mv 'node_modules' 'node_modules.$UNIX_TIME.bak'"
+fi
+# install dependencies, but not dev dependencies
+echo 'Installing production dependencies...'
+ee 'yarn --prod --frozen-lockfile --non-interactive'
+echo 'Done installing production dependencies.'
+# pack a dist.zip for AWS
+if [[ -n "$GIT_TAG" ]]; then
+    ZIP_NAME="$PACKAGE_NAME-$GIT_TAG.dist.zip"
+else
+    ZIP_NAME="$PACKAGE_NAME-$GIT_BRANCH-$GIT_SHORT_COMMIT.dist.zip"
+fi
+echo "Packing \"$ZIP_NAME\" for AWS..."
+FILES="$(cat package.json | jq -r '.files[]' | tr '\n' ' ')"
+ee "zip -r '$ZIP_NAME' ${FILES}LICENSE node_modules package.json README.md"
+echo "Done packing \"$ZIP_NAME\" for AWS."
+# restore original node_modules, if it existed
+if [[ -n "$UNIX_TIME" ]]; then
+    echo 'Restoring your node_modules folder.'
+    ee 'rm -rf node_modules'
+    ee "mv 'node_modules.$UNIX_TIME.bak' 'node_modules'"
+fi
+printf "\e[1;32mOUTPUT:\e[0;32m $(pwd)/$ZIP_NAME\e[0m\n"
+echo 'This zip folder can be uploaded directly to AWS lambda.'
+popd
+echo "Done. - ${BASH_SOURCE[0]}"
